58fcb1b0865485d393b009fdf53adab0a56e173e
pinakigroup <pinakigroup.sd@gmail.com>
2022-11-06 17:03:22 +0600

    nfjuswh oikvksmjk

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length = 64, blank = True, null = True)
+    detail = models.CharField(max_length = 32, blank = True, null = True)
+    
